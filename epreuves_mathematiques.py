e5651021078a1c33b5171a41e8e9dfa9af795497
Rouxitoo <128629800+Rouxitoo@users.noreply.github.com>
2024-12-01 13:01:26 +0100

    Update epreuves_mathematiques.py
    ajout de la seconde épreuve : Nombre premier (manque les commentaires pour plus de détail)

--- epreuves_mathematiques.py
+++ epreuves_mathematiques.py
@@ -13,10 +13,34 @@ def epreuve_math_factorielle():
     reponse_joueur=int(input("Entrez votre réponse  :"))
     bonne_reponse=factorielle(factorielle_aleatoire)
     if reponse_joueur==bonne_reponse:
-        print("Vous avez gagné une clé!!")
+        print("Bravo , Vous avez gagné une clé!!")
         return True
     else:print("Mauvaise réponse !! La bonne réponse était :",bonne_reponse,".")
     return False
 
 
-epreuve_math_factorielle()
+def est_premier(n):
+    Premier=True
+    i=2
+    while Premier and i<n:
+        if n%i==0:
+            Premier=False
+        i+=1
+    return Premier
+
+def premier_le_plus_proche(n):
+    while est_premier(n)==False:
+        n=n+1
+    return n
+
+def epreuve_math_premier():
+    nombre_aleatoire=randint(10,20)
+    print("Trouvez le nombre premier le plus proche de :",nombre_aleatoire)
+    bonne_reponse=premier_le_plus_proche(nombre_aleatoire)
+    reponse_joueur=int(input("Entrez votre réponse :"))
+    if bonne_reponse==reponse_joueur:
+        print("Bravo , Vous avez gagné une clé!!")
+        return True
+    else:
+        print("Mauvaise réponse !! La bonne réponse était :", bonne_reponse, ".")
+    return False
