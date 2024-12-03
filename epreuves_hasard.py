bd6e9676e05033cdd724f85faf9cb2d9aa20d651
Clem28L <clemloy28@gmail.com>
2024-12-03 20:12:09 +0100

    Correctif de bug dans la fonction bonneteau (boucle infini)

--- epreuves_hasard.py
+++ epreuves_hasard.py
@@ -9,25 +9,17 @@ def bonneteau():
     for element in liste_elements:
         print(element, end=" ")
     print(" ")
-    position_bonneteau = randint(0,2)
-    print(position_bonneteau)
+    bonneteau_gagnant = liste_elements[randint(0,2)]
+    print(bonneteau_gagnant)
     while tentative >0:
         print('il te reste '+str(tentative)+"tentative choisie entre les differents bonneteaux disponibles")
-        bonneteau_choisie = input()
+        bonneteau_choisie = input().upper()
         while bonneteau_choisie not in liste_elements:
             print("Choix du boneteau incorrecte veuillez en choisir un autre")
-        if liste_elements[position_bonneteau] == bonneteau_choisie:
+            bonneteau_choisie = input().upper()
+        if bonneteau_gagnant == bonneteau_choisie:
             print("Gagné tu as trouvé le bonneteau")
             return True
-        else:
-            print("Perdu")
         del liste_elements[liste_elements.index(bonneteau_choisie)]
         tentative = tentative -1
-    print("tu as perdu... le bonneteaux se trouvé sous le bonneteau "+ liste_elements[position_bonneteau])
-
-
-
-
-
-
-bonneteau()
+    print("tu as perdu... le bonneteaux se trouvé sous le bonneteau "+ liste_elements[0])
