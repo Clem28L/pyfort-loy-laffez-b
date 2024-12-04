63f1c3b3380879ee77b121c63aaded912ac7aaeb
Clem28L <clemloy28@gmail.com>
2024-12-04 21:33:54 +0100

    ajout des commentaires

--- enigme_pere_fouras.py
+++ enigme_pere_fouras.py
@@ -1,8 +1,18 @@
+"""#-----------------------------Pyfort-Enigme-pere-fouras---------------------#
+Rôle : Comporte la fonction charger enigmes et enigme pere fouras permettant de lancer une enigme
+Auteurs:Nathan Laffez/Clément Loy
+"""
+
 import json
 from random import randint
 
 
 def charger_enigmes(fichier):
+    '''
+    Parametres : fichier (Nom du fichier json)
+    Sortie : liste_enigmes (Liste des dictionnaires comprenant les questions ainsi que les reponses)
+    Rôle: permet de charger le fichier json et de retourner le dictionnaire comportant les question et reponse
+    '''
     with open(fichier, 'r', encoding='utf-8') as f:
         donnees = json.load(f)
     liste_enigmes = []
@@ -14,6 +24,11 @@ def charger_enigmes(fichier):
 
 
 def enigme_pere_fouras():
+    '''
+    Parametres : aucun parametres
+    Sortie : aucune sortie fonction principal qui permet d'afficher et d'interagir avec l'utilisateur
+    Rôle: permet de donner une enigme a l'utilisateur et qu'il puisse y repondre, le joueur a trois essaie pour trouver la reponse
+    '''
     liste_enigmes = charger_enigmes("enigmesPF.json")
     enigme = liste_enigmes[randint(0, len(liste_enigmes) - 1)]
     print(enigme["question"])
