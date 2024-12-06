e4d392844e1df9e18fa31971ab3745ae7bf70cc4
Clem28L <clemloy28@gmail.com>
2024-12-06 10:17:41 +0100

    ajout d'une fonction introduction

---
+++ fonctions_utiles.py
@@ -0,0 +1,6 @@
+
+def introduction():
+    print("#-------------------------Fort-Boyard-------------------#")
+    print("bienvenue dans l'aventure Fort-Boyard")
+    print("tu doit accomplir des épreuves pour gagner des clés et déverrouiller la salle du trésor")
+    print("L'objectif est de ramasser trois clés pour accéder à la salle du trésor")
